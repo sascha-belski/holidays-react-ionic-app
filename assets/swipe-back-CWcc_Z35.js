a888642ca348ed6326df2001a1fb0e38519226b7
sascha-belski <sascha.belski@gmail.com>
2025-07-29 08:35:37 +0200

    Updates

--- assets/swipe-back-CWcc_Z35.js
+++ assets/swipe-back-CWcc_Z35.js
@@ -1,3 +1,3 @@
-import{x as h,y as D,z as M}from"./ionic-DzVLSjYM.js";/*!
+import{x as h,y as D,z as M}from"./ionic-Cesb0jzO.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const k=(n,m,g,p,X)=>{const c=n.ownerDocument.defaultView;let s=h(n);const w=t=>{const{startX:e}=t;return s?e>=c.innerWidth-50:e<=50},a=t=>s?-t.deltaX:t.deltaX,y=t=>s?-t.velocityX:t.velocityX;return D({el:n,gestureName:"goback-swipe",gesturePriority:101,threshold:10,canStart:t=>(s=h(n),w(t)&&m()),onStart:g,onMove:t=>{const e=a(t)/c.innerWidth;p(e)},onEnd:t=>{const o=a(t),e=c.innerWidth,r=o/e,i=y(t),v=e/2,l=i>=0&&(i>.2||o>v),u=(l?1-r:r)*e;let d=0;if(u>5){const f=u/Math.abs(i);d=Math.min(f,540)}X(l,r<=0?.01:M(0,r,.9999),d)}})};export{k as createSwipeBackGesture};
