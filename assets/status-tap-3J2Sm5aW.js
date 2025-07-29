1dc3caefc2013ad122381dd8bed90ed73b0141bc
sascha-belski <sascha.belski@gmail.com>
2025-07-29 15:35:50 +0200

    Updates

--- assets/status-tap-3J2Sm5aW.js
+++ assets/status-tap-3J2Sm5aW.js
@@ -1,3 +1,3 @@
-import{F as a,G as i,H as c,J as d,K as l}from"./ionic-Bvz-ng-s.js";/*!
+import{H as a,J as i,K as c,L as d,M as l}from"./ionic-SyFi2hSQ.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
