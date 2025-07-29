837d76aa3c857702a9ffd01601bfab1e154e973e
sascha-belski <sascha.belski@gmail.com>
2025-07-29 13:27:38 +0200

    Updates

--- assets/status-tap-DFQRyLp5.js
+++ assets/status-tap-DFQRyLp5.js
@@ -1,3 +1,3 @@
-import{C as a,D as i,E as c,F as d,G as l}from"./ionic-Cesb0jzO.js";/*!
+import{F as a,G as i,H as c,J as d,K as l}from"./ionic-CCkgk1k0.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
