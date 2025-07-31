6cf63cbf7db5a4b0880c8467c4bf131857cdd8fa
sascha-belski <sascha.belski@gmail.com>
2025-07-31 06:48:36 +0200

    readme added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -36,11 +36,17 @@ const About: React.FC = () => (
     <IonContent>
       <IonCard>
         <IonCardContent>
-          <p>This is a demonstration project built using **React**, **TypeScript**, **Vite**, and **Ionic Framework** to explore public holidays by country and date.
-              **Note:** This app uses a free REST API courtesy of [openholidaysapi.org](https://openholidaysapi.org/swagger/index.html)
-              for **educational and demonstrational purposes only**.</p>
-          <p>fork the corresponding GitHub repo. Amerliorations are highly appreciated</p>
-</IonCardContent>
+          <p>This is a demonstration project built using <strong>React</strong>, <strong>TypeScript</strong>, <strong>Vite</strong>, and the <strong>Ionic Framework</strong> to explore public holidays by country and date.</p>
+
+          <p><strong>Note:</strong> This app uses a free REST API courtesy of
+          <a href="https://openholidaysapi.org/swagger/index.html" target="_blank">openholidaysapi.org</a>
+          for <strong>educational and demonstrational purposes only</strong>.</p>
+
+          <p>If you're interested, feel free to fork the corresponding GitHub repository.
+          Improvements and contributions are highly appreciated!</p>
+          <br />
+          <p>Alexander Belski 2025</p>
+        </IonCardContent>
       </IonCard>
     </IonContent>
   </IonPage>
